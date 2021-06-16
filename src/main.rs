3dcf7ed3f678b9ba3aaf5346dec1207af613457f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-06-16 16:16:19 -0400

    end of 29

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-use clap::App;
+use clap::{App, Arg};
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .help("Do not print newline")
+                .takes_value(false)
+                .short("n"),
+        )
         .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+
+    let text = vec!["Hello", "world"];
+    assert_eq!(text.join(" "), "Hello world");
+    println!("{}", text.join(" "));
 }
