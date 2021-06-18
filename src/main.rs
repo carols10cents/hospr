0bf63c6a624ea4063a29667586cf926629729e66
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-06-18 10:13:35 -0400

    got b almost working except for weird spacing

--- src/main.rs
+++ src/main.rs
@@ -19,16 +19,37 @@ fn main() {
                 .long("number")
                 .short("n"),
         )
+        .arg(
+            Arg::with_name("number_noblank")
+                .help("number nonempty output lines, overrides -n")
+                .takes_value(false)
+                .long("number-nonblank")
+                .short("b"),
+        )
         .get_matches();
 
     let files = matches.values_of_lossy("file").unwrap();
     let number = matches.is_present("number");
+    let number_noblank = matches.is_present("number_noblank");
 
     let output = files
         .iter()
         .map(|file| match fs::read_to_string(&file) {
             Ok(contents) => {
-                if number {
+                if number_noblank {
+                    let mut output = String::new();
+                    let mut line_num = 1;
+                    for line in contents.lines() {
+                        if line.is_empty() {
+                            output.push_str("\n");
+                        } else {
+                            output.push_str(&format!("{:>6}\t{}\n", line_num, line));
+                            line_num += 1;
+                        }
+                    }
+                    output = output.trim_end().to_string();
+                    output
+                } else if number {
                     contents
                         .lines()
                         .enumerate()
