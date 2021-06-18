a0728f33bd9b805941a8d5354d9c9e66785e44ac
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-06-18 09:46:55 -0400

    get bad_file test passing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use clap::App;
+use clap::{App, Arg};
+use std::fs;
 
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Carol (Nichols || Goulding)")
         .about("Rust cat")
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .help("Input file")
+                .multiple(true),
+        )
         .get_matches();
+
+    let files = matches.values_of_lossy("file").unwrap();
+
+    for file in files {
+        match fs::read_to_string(&file) {
+            Ok(contents) => println!("{}", contents),
+            Err(_) => {
+                eprintln!("\"{}\" is not a valid file.", file);
+                std::process::exit(1);
+            }
+        }
+    }
 }
