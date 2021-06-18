4b4ff2ea09dc012587e8e3056c1ce7fb4838ab4c
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-06-18 10:24:49 -0400

    got everything working except there's an extra newline

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
-use std::fs;
+use std::{
+    fs::File,
+    io::{self, Read},
+};
 
 fn main() {
     let matches = App::new("echor")
@@ -34,38 +37,55 @@ fn main() {
 
     let output = files
         .iter()
-        .map(|file| match fs::read_to_string(&file) {
-            Ok(contents) => {
-                if number_noblank {
-                    let mut output = String::new();
-                    let mut line_num = 1;
-                    for line in contents.lines() {
-                        if line.is_empty() {
-                            output.push_str("\n");
-                        } else {
-                            output.push_str(&format!("{:>6}\t{}\n", line_num, line));
-                            line_num += 1;
-                        }
+        .map(|file| {
+            let mut contents = String::new();
+            let read = if file == "-" {
+                io::stdin().read_to_string(&mut contents)
+            } else {
+                let open = File::open(file);
+                match open {
+                    Ok(mut open_file) => open_file.read_to_string(&mut contents),
+                    Err(_) => {
+                        eprintln!("\"{}\" is not a valid file.", file);
+                        std::process::exit(1);
                     }
-                    output = output.trim_end().to_string();
-                    output
-                } else if number {
-                    contents
-                        .lines()
-                        .enumerate()
-                        .map(|(line_num, line)| format!("{:>6}\t{}", line_num + 1, line))
-                        .collect::<Vec<_>>()
-                        .join("\n")
-                } else {
-                    contents
                 }
-            }
-            Err(_) => {
-                eprintln!("\"{}\" is not a valid file.", file);
-                std::process::exit(1);
+            };
+
+            match read {
+                Ok(_) => {
+                    if number_noblank {
+                        let mut output = String::new();
+                        let mut line_num = 1;
+                        for line in contents.lines() {
+                            if line.is_empty() {
+                                output.push_str("\n");
+                            } else {
+                                output.push_str(&format!("{:>6}\t{}\n", line_num, line));
+                                line_num += 1;
+                            }
+                        }
+                        output = output.trim_end().to_string();
+                        output
+                    } else if number {
+                        contents
+                            .lines()
+                            .enumerate()
+                            .map(|(line_num, line)| format!("{:>6}\t{}", line_num + 1, line))
+                            .collect::<Vec<_>>()
+                            .join("\n")
+                    } else {
+                        contents = contents.trim_end().to_string();
+                        contents
+                    }
+                }
+                Err(_) => {
+                    eprintln!("\"{}\" is not a valid file.", file);
+                    std::process::exit(1);
+                }
             }
         })
         .collect::<Vec<_>>()
         .join("\n");
-    print!("{}", output);
+    print!("{}", output.trim_end());
 }
