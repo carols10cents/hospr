b69dcfd858351e1cb43c5bb46d30ee94b848b0da
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-06-18 10:07:19 -0400

    almost got n, something weird about tab spacing on the first line

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,39 @@ fn main() {
                 .help("Input file")
                 .multiple(true),
         )
+        .arg(
+            Arg::with_name("number")
+                .help("number all output lines")
+                .takes_value(false)
+                .long("number")
+                .short("n"),
+        )
         .get_matches();
 
     let files = matches.values_of_lossy("file").unwrap();
+    let number = matches.is_present("number");
 
-    for file in files {
-        match fs::read_to_string(&file) {
-            Ok(contents) => print!("{}", contents),
+    let output = files
+        .iter()
+        .map(|file| match fs::read_to_string(&file) {
+            Ok(contents) => {
+                if number {
+                    contents
+                        .lines()
+                        .enumerate()
+                        .map(|(line_num, line)| format!("{:>6}\t{}", line_num + 1, line))
+                        .collect::<Vec<_>>()
+                        .join("\n")
+                } else {
+                    contents
+                }
+            }
             Err(_) => {
                 eprintln!("\"{}\" is not a valid file.", file);
                 std::process::exit(1);
             }
-        }
-    }
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+    print!("{}", output);
 }
