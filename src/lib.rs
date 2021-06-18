5226561a79f751f8a47f3a8e60f144e0850a311e
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-06-18 16:18:56 -0400

    top66

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust head")
+        .get_matches();
+    Ok(Config {
+        lines,
+        bytes,
+        files,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
