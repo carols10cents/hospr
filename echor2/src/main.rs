fe3950b691698bce5e50b5d468d23f97ed993aca
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-13 19:53:29 -0400

    28

--- echor2/src/main.rs
+++ echor2/src/main.rs
@@ -1,8 +1,23 @@
-use clap::App;
+use clap::{App, Arg};
+
 fn main() {
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .help("Do not print newline")
+                .takes_value(false)
+                .short("n"),
+        )
         .get_matches();
+    println!("{:#?}", matches);
 }
