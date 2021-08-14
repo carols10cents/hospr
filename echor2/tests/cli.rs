0f8d4521349a4a4caf3fb36e80850344383d9917
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-13 20:22:04 -0400

    36

--- echor2/tests/cli.rs
+++ echor2/tests/cli.rs
@@ -1,8 +1,6 @@
-use std::process::Command;
-
+use assert_cmd::Command;
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
-    assert!(res.is_ok());
+    let mut cmd = Command::cargo_bin("echor2").unwrap();
+    cmd.arg("hello").assert().success();
 }
