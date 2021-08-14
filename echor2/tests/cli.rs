06305e37571a66fc73bb1af4dea26297a224a9ae
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-13 20:27:47 -0400

    39

--- echor2/tests/cli.rs
+++ echor2/tests/cli.rs
@@ -1,9 +1,19 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
+
 #[test]
 fn dies_no_args() {
     let mut cmd = Command::cargo_bin("echor2").unwrap();
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+}
+
+#[test]
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor2").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
