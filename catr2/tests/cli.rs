3768a8401fca2b9768c92eceee2437d1eab18067
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-14 14:09:44 -0400

    51

--- catr2/tests/cli.rs
+++ catr2/tests/cli.rs
@@ -64,11 +64,7 @@ fn run(args: &[&str], expected_file: &str) -> TestResult {
 }
 
 // --------------------------------------------------
-fn run_stdin(
-    input_file: &str,
-    args: &[&str],
-    expected_file: &str,
-) -> TestResult {
+fn run_stdin(input_file: &str, args: &[&str], expected_file: &str) -> TestResult {
     let input = fs::read_to_string(input_file)?;
     let expected = fs::read_to_string(expected_file)?;
     Command::cargo_bin(PRG)?
