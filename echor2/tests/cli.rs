9f2beb6e86956d4125b3612021351236827a08a3
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-13 20:34:37 -0400

    top 42

--- echor2/tests/cli.rs
+++ echor2/tests/cli.rs
@@ -2,6 +2,7 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
 fn dies_no_args() -> TestResult {
     let mut cmd = Command::cargo_bin("echor")?;
@@ -10,10 +11,22 @@ fn dies_no_args() -> TestResult {
         .stderr(predicate::str::contains("USAGE"));
     Ok(())
 }
+
 #[test]
 fn hello1() -> TestResult {
     let expected = fs::read_to_string("tests/expected/hello1.txt")?;
     let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello2.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(vec!["Hello", "there"])
+        .assert()
+        .success()
+        .stdout(expected);
     Ok(())
 }
