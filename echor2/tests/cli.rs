4c241cb49426d75744b92195db76fee6b07f1551
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-13 20:33:48 -0400

    41

--- echor2/tests/cli.rs
+++ echor2/tests/cli.rs
@@ -1,19 +1,19 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
-
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
-
 #[test]
-fn hello1() {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn hello1() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
