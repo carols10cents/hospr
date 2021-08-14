efaba8827d560d2456e9e80305a428c0eb716e8e
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-13 20:06:18 -0400

    33

--- echor2/src/main.rs
+++ echor2/src/main.rs
@@ -19,5 +19,9 @@ fn main() {
                 .short("n"),
         )
         .get_matches();
-    println!("{:#?}", matches);
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+
+    let ending = if omit_newline { "" } else { "\n" };
+    print!("{}{}", text.join(" "), ending);
 }
