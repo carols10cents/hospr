1f5285d6abdb796f069e7c48f47c73159c4c9467
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-14 14:14:30 -0400

    top 53

--- catr2/src/lib.rs
+++ catr2/src/lib.rs
@@ -2,6 +2,13 @@ use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
 pub fn run() -> MyResult<()> {
     println!("Hello, world!");
     Ok(())
