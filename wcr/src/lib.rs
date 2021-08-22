66b539d1a73db2f502ba5835a6a837eabddd5db6
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-21 20:33:03 -0400

    help text

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -17,7 +17,39 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust wc")
-        // What goes here?
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .conflicts_with("chars")
+                .help("Show byte count"),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help("Show character count"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .help("Show line count"),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .help("Show word count"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .required(true)
+                .default_value("-")
+                .min_values(1),
+        )
         .get_matches();
 
     Ok(Config {
