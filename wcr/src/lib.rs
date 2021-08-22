1796188d19e717af615803e5de60e5cc68082881
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-22 09:01:39 -0400

    106

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -178,3 +178,22 @@ pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
         num_chars,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
+}
