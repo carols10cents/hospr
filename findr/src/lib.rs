4f0fef7ffa28c95ddf6136d6954518c63d0f5355
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-22 21:38:59 -0400

    146

---
+++ findr/src/lib.rs
@@ -0,0 +1,20 @@
+use crate::EntryType::*;
+use clap::{App, Arg};
+use regex::Regex;
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, PartialEq)]
+enum EntryType {
+    Dir,
+    File,
+    Link,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    dirs: Vec<String>,
+    names: Option<Vec<Regex>>,
+    entry_types: Option<Vec<EntryType>>,
+}
