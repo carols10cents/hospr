ea8cd48ddd981d0956fe06fa375b121c21e33b0a
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-26 20:36:07 -0400

    framework

---
+++ cutr/src/lib.rs
@@ -0,0 +1,41 @@
+use crate::Extract::*;
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+type PositionList = Vec<usize>;
+
+#[derive(Debug)]
+pub enum Extract {
+    Fields(PositionList),
+    Bytes(PositionList),
+    Chars(PositionList),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    delimiter: u8,
+    extract: Extract,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("cutr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust cut")
+        // What goes here?
+        .get_matches();
+    Ok(Config {
+        files,
+        delimiter,
+        fields,
+        bytes,
+        chars,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", &config);
+    Ok(())
+}
