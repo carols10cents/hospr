9337b44fcee8251bdf1f42058b7ecca5112d5cad
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-28 20:49:01 -0400

    179

--- csv-usage/src/main.rs
+++ csv-usage/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use csv::StringRecord;
+use std::fs::File;
+fn main() -> std::io::Result<()> {
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b',')
+        .from_reader(File::open("books.csv")?);
+    fmt(reader.headers()?);
+    for record in reader.records() {
+        fmt(&record?);
+    }
+    Ok(())
+}
+fn fmt(rec: &StringRecord) {
+    println!(
+        "{}",
+        rec.into_iter()
+            .map(|v| format!("{:20}", v))
+            .collect::<Vec<String>>()
+            .join("")
+    )
 }
