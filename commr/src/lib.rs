aa7edcb5133c204f5d38b33b85c6c0ffc0f7e90b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-30 16:35:35 -0400

    get args and help text working, mostly

--- commr/src/lib.rs
+++ commr/src/lib.rs
@@ -19,8 +19,59 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust comm")
-        // What goes here?
+        .arg(
+            Arg::with_name("file1")
+                .value_name("FILE1")
+                .help("Input file 1")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("file2")
+                .value_name("FILE2")
+                .help("Input file 2")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("suppress-col1")
+                .help("Suppress printing of column 1")
+                .short("1")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("suppress-col2")
+                .help("Suppress printing of column 2")
+                .short("2")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("suppress-col3")
+                .help("Suppress printing of column 3")
+                .short("3")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("insensitive")
+                .help("Case insensitive comparison of lines")
+                .short("i")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("delimiter")
+                .value_name("DELIM")
+                .help("Output delimiter")
+                .short("d")
+                .long("output-delimiter")
+                .default_value("\t"),
+        )
         .get_matches();
+
+    let file1 = matches.value_of("file1").unwrap().to_string();
+    let file2 = matches.value_of("file2").unwrap().to_string();
+    let suppress_col1 = matches.is_present("suppress-col1");
+    let suppress_col2 = matches.is_present("suppress-col2");
+    let suppress_col3 = matches.is_present("suppress-col3");
+    let insensitive = matches.is_present("insensitive");
+    let delimiter = matches.value_of("delimiter").unwrap().to_string();
 
     Ok(Config {
         file1,
