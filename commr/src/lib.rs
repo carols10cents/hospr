f251967c5981427782cb5d5f71896188ec245eb4
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-30 16:17:14 -0400

    215

--- commr/src/lib.rs
+++ commr/src/lib.rs
@@ -13,3 +13,27 @@ pub struct Config {
     insensitive: bool,
     delimiter: String,
 }
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("commr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust comm")
+        // What goes here?
+        .get_matches();
+
+    Ok(Config {
+        file1,
+        file2,
+        suppress_col1,
+        suppress_col2,
+        suppress_col3,
+        insensitive,
+        delimiter,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
