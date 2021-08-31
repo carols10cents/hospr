03db4901c6baaed51438be76135e216a1bae29ad
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-31 19:57:42 -0400

    235

---
+++ tailr2/src/lib.rs
@@ -0,0 +1,12 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: i64,
+    bytes: Option<i64>,
+    quiet: bool,
+}
