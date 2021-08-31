f4e1d5e99d82a04fe52ba72e946fbad1e759675d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-31 19:59:22 -0400

    236

--- tailr2/src/lib.rs
+++ tailr2/src/lib.rs
@@ -10,3 +10,24 @@ pub struct Config {
     bytes: Option<i64>,
     quiet: bool,
 }
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("tailr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust tail")
+        // What goes here?
+        .get_matches();
+
+    Ok(Config {
+        lines,
+        bytes,
+        files,
+        quiet,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
