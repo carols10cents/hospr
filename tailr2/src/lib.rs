978d6dedefb2be934768dec45dd5e0c00b5e74a9
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-08-31 20:09:03 -0400

    args and help

--- tailr2/src/lib.rs
+++ tailr2/src/lib.rs
@@ -16,14 +16,54 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust tail")
-        // What goes here?
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .takes_value(true)
+                .min_values(1)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .conflicts_with("lines"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("quiet")
+                .short("q")
+                .long("quiet")
+                .takes_value(false)
+                .help("Suppress headers"),
+        )
         .get_matches();
+
+    let bytes = match matches.value_of("bytes") {
+        None => None,
+        Some(b) => Some(b.parse().map_err(|_| format!("illegal offset -- {}", b))?),
+    };
+
+    let lines = matches.value_of("lines").unwrap();
+    let lines = lines
+        .parse()
+        .map_err(|_| format!("illegal offset -- {}", lines))?;
 
     Ok(Config {
         lines,
         bytes,
-        files,
-        quiet,
+        files: matches.values_of_lossy("file").unwrap(),
+        quiet: matches.is_present("quiet"),
     })
 }
 
