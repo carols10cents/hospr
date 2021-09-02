cd6610820a6a9f9e86a0d38d05b867db1f47e02f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-02 16:39:19 -0400

    mid 246

--- tailr2/src/lib.rs
+++ tailr2/src/lib.rs
@@ -81,129 +81,14 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    let multiple_files = config.files.len() > 1;
-
-    for (file_num, filename) in config.files.into_iter().enumerate() {
+    for filename in config.files {
         match File::open(&filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
             Ok(file) => {
-                let mut file = BufReader::new(file);
-                if multiple_files {
-                    println!(
-                        "{}==> {} <==",
-                        if file_num == 0 { "" } else { "\n" },
-                        filename
-                    );
-                }
-
-                if let Some(num_bytes) = config.bytes {
-                    // Get the file size so we know if we've gone past the end
-                    let total_len = file.seek(SeekFrom::End(0)).unwrap();
-
-                    if num_bytes > 0 {
-                        // First seek past the number of bytes to skip
-                        let new_pos = file.seek(SeekFrom::Start(num_bytes as u64 - 1)).unwrap();
-
-                        // As long as we aren't past the end
-                        if new_pos < total_len {
-                            // Print the rest
-                            let mut line = vec![];
-                            loop {
-                                let bytes = file.read_until('\n' as u8, &mut line)?;
-                                if bytes == 0 {
-                                    break;
-                                }
-                                print!("{}", String::from_utf8_lossy(&line));
-                                line.clear();
-                            }
-                        }
-                    } else {
-                        // if seeking this many bytes would take us past the beginning, start at
-                        // the beginning instead.
-                        if total_len as i64 + num_bytes < 0 {
-                            file.seek(SeekFrom::Start(0)).unwrap();
-                        } else {
-                            file.seek(SeekFrom::End(num_bytes)).unwrap();
-                        }
-                        // Print the rest
-                        let mut line = vec![];
-                        loop {
-                            let bytes = file.read_until('\n' as u8, &mut line)?;
-                            if bytes == 0 {
-                                break;
-                            }
-                            print!("{}", String::from_utf8_lossy(&line));
-                            line.clear();
-                        }
-                    }
-                } else {
-                    if config.lines > 0 {
-                        let mut line = String::new();
-
-                        // First skip config.lines
-                        for _ in 0..config.lines - 1 {
-                            let bytes = file.read_line(&mut line)?;
-                            if bytes == 0 {
-                                break;
-                            }
-                            line.clear();
-                        }
-
-                        // Then print the rest
-                        loop {
-                            let bytes = file.read_line(&mut line)?;
-                            if bytes == 0 {
-                                break;
-                            }
-                            print!("{}", line);
-                            line.clear();
-                        }
-                    } else {
-                        if let Err(_) = file.seek(SeekFrom::End(-1)) {
-                            // if the file is empty, go to the next file
-                            continue;
-                        }
-
-                        // First seek back abs(config.lines) number of lines
-                        let mut lines_found = 1; // lol magic number
-
-                        loop {
-                            // read one byte
-                            let mut byte_buf: Vec<u8> = vec![0];
-                            file.read_exact(&mut byte_buf).unwrap();
-
-                            // if we found a newline, count it
-                            if byte_buf[0] == '\n' as u8 {
-                                lines_found -= 1;
-                                if lines_found == config.lines {
-                                    break;
-                                }
-                            }
-
-                            // go back the byte we just read and the byte before that
-                            if let Err(_) = file.seek(SeekFrom::Current(-2)) {
-                                // if we're at the beginning of the file, stop
-                                file.seek(SeekFrom::Current(-1)).unwrap();
-                                break;
-                            }
-                        }
-
-                        // Then print
-                        let mut line = String::new();
-                        loop {
-                            let bytes = file.read_line(&mut line)?;
-                            if bytes == 0 {
-                                break;
-                            }
-                            print!("{}", line);
-                            line.clear();
-                        }
-                    }
-                }
+                print_lines(BufReader::new(file), config.lines)?;
             }
         }
     }
-
     Ok(())
 }
 
