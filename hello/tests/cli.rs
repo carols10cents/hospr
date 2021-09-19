aa7fd94984187cb505ea9fdd3a3cd38440fabc1c
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-18 21:09:21 -0400

    27

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
+    let mut cmd = Command::new("hello");
     let res = cmd.output();
     assert!(res.is_ok());
 }
