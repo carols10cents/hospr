668c3832e10a7bb3c0cc1201733da9f77594a9e6
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-18 21:07:05 -0400

    26

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,4 +1,8 @@
+use std::process::Command;
+
 #[test]
-fn works() {
-    assert!(false);
+fn runs() {
+    let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    assert!(res.is_ok());
 }
