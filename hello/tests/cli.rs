4743af8992e2f5fa55a58239e9deb9da28a04c5c
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-19 21:09:59 -0400

    mid 32

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -3,7 +3,7 @@ use assert_cmd::Command;
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
-    cmd.assert().success();
+    cmd.assert().success().stdout("Hello, world!\n");
 }
 
 #[test]
