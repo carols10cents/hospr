929009b77291d2cbb64f482999ed372f3c3d85b8
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-19 21:08:32 -0400

    31

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -11,3 +11,9 @@ fn true_ok() {
     let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
