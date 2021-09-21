7d091cb67a86c7fd1c56a2465e0457a4fe4fad27
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-20 21:54:59 -0400

    get my code working on 287

--- fortuner/src/lib.rs
+++ fortuner/src/lib.rs
@@ -16,11 +16,47 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust fortune")
-        // What goes here?
+        .arg(
+            Arg::with_name("pattern")
+                .short("m")
+                .long("pattern")
+                .takes_value(true)
+                .value_name("PATTERN")
+                .help("Search pattern"),
+        )
+        .arg(
+            Arg::with_name("seed")
+                .short("s")
+                .long("seed")
+                .takes_value(true)
+                .value_name("SEED")
+                .help("PRNG seed"),
+        )
+        .arg(
+            Arg::with_name("sources")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .required(true)
+                .min_values(1),
+        )
         .get_matches();
 
+    let pattern = match matches.value_of("pattern") {
+        Some(pat) => Some(
+            RegexBuilder::new(pat)
+                .build()
+                .map_err(|_| format!("Invalid --pattern \"{}\"", pat))?,
+        ),
+        None => None,
+    };
+
+    let seed = match matches.value_of("seed") {
+        Some(s) => Some(parse_u64(s)?),
+        None => None,
+    };
+
     Ok(Config {
-        sources,
+        sources: matches.values_of_lossy("sources").unwrap(),
         seed,
         pattern,
     })
@@ -32,7 +68,9 @@ pub fn run(config: Config) -> MyResult<()> {
 }
 
 fn parse_u64(val: &str) -> MyResult<u64> {
-    unimplemented!();
+    Ok(val
+        .parse()
+        .map_err(|_| format!("\"{}\" not a valid integer", val))?)
 }
 
 #[cfg(test)]
