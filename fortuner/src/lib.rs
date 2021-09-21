4a0042eb8c1c41bf3d2f40266a052f39761dd834
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-20 20:38:47 -0400

    boilerplate, fixed

---
+++ fortuner/src/lib.rs
@@ -0,0 +1,32 @@
+use clap::{App, Arg};
+use regex::{Regex, RegexBuilder};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    sources: Vec<String>,
+    pattern: Option<Regex>,
+    seed: Option<u64>,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("fortuner")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust fortune")
+        // What goes here?
+        .get_matches();
+
+    Ok(Config {
+        sources,
+        seed,
+        pattern,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
