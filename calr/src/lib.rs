c1d8892976f06e3c820c268902a6ac245fcbbbe5
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-09-26 20:32:01 -0400

    arg parsing and defaults

--- calr/src/lib.rs
+++ calr/src/lib.rs
@@ -1,3 +1,4 @@
+use chrono::{Datelike, Local};
 use clap::{App, Arg};
 use std::error::Error;
 
@@ -14,8 +15,37 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust cal")
-        // What goes here?
+        .arg(
+            Arg::with_name("year")
+                .short("y")
+                .long("year")
+                .takes_value(false)
+                .help("Show whole current year"),
+        )
+        .arg(
+            Arg::with_name("month")
+                .short("m")
+                .value_name("MONTH")
+                .help("Month name or number (1-12)"),
+        )
+        .arg(
+            Arg::with_name("year_value")
+                .value_name("YEAR")
+                .help("Year (1-9999)"),
+        )
         .get_matches();
+
+    let now = Local::now();
+
+    let month = match matches.value_of("month") {
+        Some(m) => Some(m.parse::<u32>()?),
+        None => Some(now.month()),
+    };
+    let year = matches
+        .value_of("year_value")
+        .map(|y| y.parse())
+        .transpose()?
+        .unwrap_or(now.year());
 
     Ok(Config { month, year })
 }
