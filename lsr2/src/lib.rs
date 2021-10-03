bc5cf66767605d08ee2e5c067561f7a0833ceab6
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-02 20:14:49 -0400

    my get args is working

--- lsr2/src/lib.rs
+++ lsr2/src/lib.rs
@@ -15,8 +15,32 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust ls")
-        // What goes here?
+        .arg(
+            Arg::with_name("paths")
+                .value_name("PATH")
+                .help("Files and/or directories")
+                .default_value(".")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("all")
+                .short("a")
+                .long("all")
+                .help("Show all files")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("long")
+                .short("l")
+                .long("long")
+                .help("Long listing")
+                .takes_value(false),
+        )
         .get_matches();
+
+    let paths = matches.values_of_lossy("paths").unwrap();
+    let long = matches.is_present("long");
+    let show_hidden = matches.is_present("all");
 
     Ok(Config {
         paths,
