62fd298c54e573b8b92c37e5cafd95e876ff2d48
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-08 21:12:52 -0400

    oops, only print error, not return error

--- lsr2/src/lib.rs
+++ lsr2/src/lib.rs
@@ -57,7 +57,13 @@ fn find_files(paths: &[String], show_hidden: bool) -> MyResult<Vec<PathBuf>> {
     let mut files = vec![];
 
     for path in paths {
-        let metadata = fs::metadata(path).map_err(|e| format!("{}: {}", path, e))?;
+        let metadata = match fs::metadata(path) {
+            Ok(m) => m,
+            Err(e) => {
+                eprintln!("{}: {}", path, e);
+                continue;
+            }
+        };
         if metadata.is_dir() {
             for entry in fs::read_dir(path)? {
                 let entry = entry?;
