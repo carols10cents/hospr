d85f9182f925a80da6b6e17247c36578f0b147f8
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-15 21:46:34 -0400

    269

--- tailr3/src/lib.rs
+++ tailr3/src/lib.rs
@@ -17,3 +17,24 @@ pub struct Config {
     bytes: Option<TakeValue>,
     quiet: bool,
 }
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("tailr")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust tail")
+        // What goes here?
+        .get_matches();
+
+    Ok(Config {
+        files: unimplemented!(),
+        lines: unimplemented!(),
+        bytes: unimplemented!(),
+        quiet: unimplemented!(),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
