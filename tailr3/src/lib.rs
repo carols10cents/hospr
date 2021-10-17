ac4277ebd68341d8f0e3859fef6987d4a3689e12
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-16 20:21:47 -0400

    283

--- tailr3/src/lib.rs
+++ tailr3/src/lib.rs
@@ -3,6 +3,7 @@ use clap::{App, Arg};
 use regex::Regex;
 use std::error::Error;
 use std::fs::File;
+use std::io::BufRead;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -93,6 +94,14 @@ pub fn run(config: Config) -> MyResult<()> {
     Ok(())
 }
 
+fn print_lines(mut file: impl BufRead, num_lines: &TakeValue, total_lines: i64) -> MyResult<()> {
+    unimplemented!();
+}
+
+fn get_start_index(take_val: &TakeValue, total: i64) -> Option<u64> {
+    unimplemented!();
+}
+
 fn parse_num(val: &str) -> MyResult<TakeValue> {
     let signs: &[char] = &['+', '-'];
     let res = val
@@ -117,7 +126,7 @@ fn count_lines_bytes(filename: &str) -> MyResult<(i64, i64)> {
 
 #[cfg(test)]
 mod tests {
-    use super::{count_lines_bytes, parse_num, TakeValue::*};
+    use super::{count_lines_bytes, get_start_index, parse_num, TakeValue::*};
 
     #[test]
     fn test_count_lines_bytes() {
@@ -182,5 +191,30 @@ mod tests {
         let res = parse_num("foo");
         assert!(res.is_err());
         assert_eq!(res.unwrap_err().to_string(), "foo");
+    }
+
+    #[test]
+    fn test_get_start_index() {
+        // +0 from an empty file (0 lines/bytes) returns None
+        assert_eq!(get_start_index(&PlusZero, 0), None);
+        // +0 from a nonempty file returns an index that
+        // is one less than the number of lines/bytes
+        assert_eq!(get_start_index(&PlusZero, 1), Some(0));
+        // Taking 0 lines/bytes returns None
+        assert_eq!(get_start_index(&TakeNum(0), 1), None);
+        // Taking any lines/bytes from an empty file returns None
+        assert_eq!(get_start_index(&TakeNum(1), 0), None);
+        // Taking more lines/bytes than is available returns None
+        assert_eq!(get_start_index(&TakeNum(2), 1), None);
+        // When starting line/byte is less than total lines/bytes,
+        // return one less than starting number
+        assert_eq!(get_start_index(&TakeNum(1), 10), Some(0));
+        assert_eq!(get_start_index(&TakeNum(2), 10), Some(1));
+        assert_eq!(get_start_index(&TakeNum(3), 10), Some(2));
+        // When starting line/byte is negative and less than total,
+        // return total - start
+        assert_eq!(get_start_index(&TakeNum(-1), 10), Some(9));
+        assert_eq!(get_start_index(&TakeNum(-2), 10), Some(8));
+        assert_eq!(get_start_index(&TakeNum(-3), 10), Some(7));
     }
 }
