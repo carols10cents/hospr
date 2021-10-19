a29f806387a5f4b8e2c66ae6743ddc7df45d65f5
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-19 19:40:06 -0400

    300

---
+++ fortuner2/src/lib.rs
@@ -0,0 +1,12 @@
+use clap::{App, Arg};
+use regex::{Regex, RegexBuilder};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    sources: Vec<String>,
+    pattern: Option<Regex>,
+    seed: Option<u64>,
+}
