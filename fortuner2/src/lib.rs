72c9873200dcf919a5ac95d948d31dc5ad708654
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-19 19:41:02 -0400

    301

--- fortuner2/src/lib.rs
+++ fortuner2/src/lib.rs
@@ -10,3 +10,23 @@ pub struct Config {
     pattern: Option<Regex>,
     seed: Option<u64>,
 }
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("fortuner")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust fortune")
+        // What goes here?
+        .get_matches();
+
+    Ok(Config {
+        sources: unimplemented!(),
+        seed: unimplemented!(),
+        pattern: unimplemented!(),
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
