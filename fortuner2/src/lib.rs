f115cb1542ab3f17aa794f40e07f780306566cc7
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-19 20:04:27 -0400

    307

--- fortuner2/src/lib.rs
+++ fortuner2/src/lib.rs
@@ -1,6 +1,8 @@
 use clap::{App, Arg};
+use itertools::Itertools;
 use regex::{Regex, RegexBuilder};
 use std::error::Error;
+use std::path::PathBuf;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -16,18 +18,54 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Ken Youens-Clark <kyclark@gmail.com>")
         .about("Rust fortune")
-        // What goes here?
+        .arg(
+            Arg::with_name("sources")
+                .value_name("FILE")
+                .multiple(true)
+                .required(true)
+                .help("Input files or directories"),
+        )
+        .arg(
+            Arg::with_name("pattern")
+                .value_name("PATTERN")
+                .short("m")
+                .long("pattern")
+                .help("Pattern"),
+        )
+        .arg(
+            Arg::with_name("insensitive")
+                .short("i")
+                .long("insensitive")
+                .help("Case-insensitive pattern matching")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("seed")
+                .value_name("SEED")
+                .short("s")
+                .long("seed")
+                .help("Random seed"),
+        )
         .get_matches();
-
+    let pattern = matches
+        .value_of("pattern")
+        .map(|val| {
+            RegexBuilder::new(val)
+                .case_insensitive(matches.is_present("insensitive"))
+                .build()
+                .map_err(|_| format!("Invalid --pattern \"{}\"", val))
+        })
+        .transpose()?;
     Ok(Config {
-        sources: unimplemented!(),
-        seed: unimplemented!(),
-        pattern: unimplemented!(),
+        sources: matches.values_of_lossy("sources").unwrap(),
+        seed: matches.value_of("seed").map(parse_u64).transpose()?,
+        pattern,
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    let files = find_files(&config.sources)?;
+    println!("{:#?}", files);
     Ok(())
 }
 
@@ -35,9 +73,13 @@ fn parse_u64(val: &str) -> MyResult<u64> {
     unimplemented!();
 }
 
+fn find_files(sources: &[String]) -> MyResult<Vec<PathBuf>> {
+    unimplemented!();
+}
+
 #[cfg(test)]
 mod tests {
-    use super::parse_u64;
+    use super::{find_files, parse_u64};
 
     #[test]
     fn test_parse_u64() {
@@ -50,5 +92,46 @@ mod tests {
         let res = parse_u64("4");
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), 4);
+    }
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let res = find_files(&["./tests/inputs/jokes".to_string()]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 1);
+        assert_eq!(
+            files.get(0).unwrap().to_string_lossy(),
+            "./tests/inputs/jokes"
+        );
+        // Fails to find a bad file
+        let res = find_files(&["/path/does/not/exist".to_string()]);
+        assert!(res.is_err());
+        // Finds all the input files, excludes ".dat"
+        let res = find_files(&["./tests/inputs".to_string()]);
+        assert!(res.is_ok());
+        // Check number and order of files
+        let files = res.unwrap();
+        assert_eq!(files.len(), 5);
+        let first = files.get(0).unwrap().display().to_string();
+        assert!(first.contains("ascii-art"));
+        let last = files.last().unwrap().display().to_string();
+        assert!(last.contains("quotes"));
+        // Test for multiple sources, path must be unique and sorted
+        let res = find_files(&[
+            "./tests/inputs/jokes".to_string(),
+            "./tests/inputs/ascii-art".to_string(),
+            "./tests/inputs/jokes".to_string(),
+        ]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 2);
+        if let Some(filename) = files.first().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+        }
+        if let Some(filename) = files.last().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+        }
     }
 }
