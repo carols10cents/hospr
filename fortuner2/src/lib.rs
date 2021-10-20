e16bcf333fc36d1628567616b0c751c321631f51
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2021-10-19 20:15:43 -0400

    312

--- fortuner2/src/lib.rs
+++ fortuner2/src/lib.rs
@@ -76,7 +76,13 @@ pub fn get_args() -> MyResult<Config> {
 pub fn run(config: Config) -> MyResult<()> {
     let files = find_files(&config.sources)?;
     let fortunes = read_fortunes(&files)?;
-    println!("{:#?}", pick_fortune(&fortunes, &config.seed));
+    if let Some(pattern) = config.pattern {
+        for fortune in fortunes {
+            // Print all the fortunes matching the pattern
+        }
+    } else {
+        // Select and print one fortune
+    }
     Ok(())
 }
 
